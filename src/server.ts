d3ce368f3cf6e96287232e46a3bcd53b67a31d59
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-10-18 18:23:37 -0300

    adding routes and controllers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import  express  from "express";
 import dotenv from "dotenv";
 import mustache from 'mustache-express';
 import path from 'path';
+import router from './routes/index';
 
 
 dotenv.config();
@@ -15,6 +16,13 @@ server.engine('mustache', mustache());
 server.use(express.static(path.join(__dirname,'../public')));
 
 //rotas
+server.use(router);
 
+server.use((req,res)=>{
+        res.send("Página não encontrada!");
+})
 
-server.listen(process.env.PORT);
+require('dotenv/config');
+server.listen(process.env.PORT,()=>{
+    console.log("App rodando!");
+})
